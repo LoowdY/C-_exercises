7744774b5c500dbf34d88537d4f17f6cafe0e42d
João Renan <102002118+LoowdY@users.noreply.github.com>
2022-09-08 15:14:25 -0300

    Add files via upload

---
+++ OO-cpp.cpp
@@ -0,0 +1,32 @@
+//Programação Orientada a Objeto em C++
+
+/*
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+class Caixa
+{
+	public:
+		int comprimento;
+		int largura;
+		int altura;
+		
+		void Area();
+		void Volume();		
+};
+
+int main()
+{
+		
+	Caixa sorvete;
+	
+	cout << "Digite o comprimento da caixa: ";
+	cin >> sorvete.comprimento;
+	
+	return 0;
+	
+}
+*/
+
